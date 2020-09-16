a5dba08f7ff3c58c493228b315a1c2895dc4d24e
Roman <babanov.js@gmail.com>
2020-09-16 11:56:11 +0300

    wrote performance functions

--- src/index.js
+++ src/index.js
@@ -8,24 +8,46 @@ window.onload = function() {
   const dateFNS = format(new Date(), 'dd.MM.yyyy')
   const momentDate = moment(new Date()).format('DD.MM.yyyy');
 
-  const range = 10000
+  const range = 1000000
 
 
-  console.time('luxon')
-  for (let i =0; i < range; i ++ ) {
-    console.log(DateTime.local().toLocaleString(DateTime.DATE_SHORT))
+  const button = document.createElement('button')
+  button.innerText = 'START'
+
+  button.addEventListener('click', function () {
+    startPerformance()
+  })
+
+  document.body.appendChild(button)
+
+  function startPerformance() {
+    startDateFnsPerformance()
+    startLuxonPerformance()
+    startMomentPerformance()
   }
-  console.timeEnd('luxon')
 
-  // console.time('dateFNS')
-  // for (let i =0; i < range; i ++ ) {
-  //   console.log(format(new Date(), 'dd.MM.yyyy'))
-  // }
-  // console.timeEnd('dateFNS')
-  //
-  // console.time('moment')
-  // for (let i =0; i < range; i ++ ) {
-  //   console.log(moment(new Date()).format('DD.MM.yyyy'))
-  // }
-  // console.timeEnd('moment')
+  function startLuxonPerformance() {
+    console.time('luxon')
+    for (let i =0; i < range; i ++ ) {
+      DateTime.local().toLocaleString(DateTime.DATE_SHORT)
+    }
+    console.timeEnd('luxon')
+  }
+
+  function startDateFnsPerformance() {
+    console.time('dateFNS')
+    for (let i =0; i < range; i ++ ) {
+      format(new Date(), 'dd.MM.yyyy')
+    }
+    console.timeEnd('dateFNS')
+  }
+
+  function startMomentPerformance() {
+
+    console.time('moment')
+    for (let i =0; i < range; i ++ ) {
+      moment(new Date()).format('DD.MM.yyyy')
+    }
+    console.timeEnd('moment')
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new HtmlWebpackPlugin()]
 };
